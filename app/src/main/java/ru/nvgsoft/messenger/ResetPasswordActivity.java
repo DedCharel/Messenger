b31f093be4e00a75082995d48943cf9689cc7b2c
Daniil Shcherbakov <Charel@bk.ru>
2024-05-18 12:27:28 +0300

    Add resetPassword and Registration logic

--- app/src/main/java/ru/nvgsoft/messenger/ResetPasswordActivity.java
+++ app/src/main/java/ru/nvgsoft/messenger/ResetPasswordActivity.java
@@ -1,6 +1,8 @@
 package ru.nvgsoft.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
@@ -15,18 +18,49 @@ public class ResetPasswordActivity extends AppCompatActivity {
     private static final String EXTRAS_EMAIL = "email";
     private EditText editTextEmailForgotPassword;
     private Button buttonResetPassword;
+    private ResetPasswordViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
         initViews();
+        viewModel = new ViewModelProvider(this).get(ResetPasswordViewModel.class);
+        observeViewModel();
         String email = getIntent().getStringExtra(EXTRAS_EMAIL);
         editTextEmailForgotPassword.setText(email);
         buttonResetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = editTextEmailForgotPassword.getText().toString().trim();
+                viewModel.resetPassword(email);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(
+                            ResetPasswordActivity.this,
+                            errorMessage,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+        viewModel.isSuccess().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean success) {
+                if (success) {
+                    Toast.makeText(
+                            ResetPasswordActivity.this,
+                            R.string.reset_link_sent,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
             }
         });
     }
